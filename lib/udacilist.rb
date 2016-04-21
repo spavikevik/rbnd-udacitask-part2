8ecf2b45e9f35b7b161c580711270a7071d1142b
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-04-21 09:34:16 +0200

    Wrote format_description method in Module Listable and accordingly updated classes Link, Event and Todo.

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -5,15 +5,18 @@ class UdaciList
     @title = options[:title]
     @items = []
   end
+
   def add(type, description, options={})
     type = type.downcase
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
   end
+
   def delete(index)
     @items.delete_at(index - 1)
   end
+  
   def all
     puts "-" * @title.length
     puts @title
